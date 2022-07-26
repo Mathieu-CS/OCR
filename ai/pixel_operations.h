ed4ccdbd2d045225959bce889a30d904031f59ce
Mathieu-CS <78751980+Mathieu-CS@users.noreply.github.com>
2022-07-26 23:35:13 +0200

    Update pixel_operations.h
    removed epita

--- ai/pixel_operations.h
+++ ai/pixel_operations.h
@@ -5,7 +5,5 @@
 #include <SDL.h>
 
 Uint32 get_pixel(SDL_Surface *surface, unsigned x, unsigned y);
-//void put_pixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
-//void update_surface(SDL_Surface* screen, SDL_Surface* image);
 
 #endif
