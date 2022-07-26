bd79e7cd8467798cb3b7a12de68425756d8152cc
Mathieu-CS <78751980+Mathieu-CS@users.noreply.github.com>
2022-07-26 23:45:22 +0200

    Update hough_transform.h
    removed commented code

--- image_preprocessing/hough_transform.h
+++ image_preprocessing/hough_transform.h
@@ -4,10 +4,6 @@
 #include "SDL/SDL.h"
 #include <math.h>
 
-/*int setPixelVerif(SDL_Surface *Screen,int x, int y);
-
-void drawLine(SDL_Surface *Screen, int x0, int y0, int x1, int y1, Uint32 pixel);*/
-
 double Convert(int degree);
 
 void edge_detection(char* path);
